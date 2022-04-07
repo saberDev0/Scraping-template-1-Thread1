ac1c7e3a42bce8857bbbbfb6f0a5a9ad1f728faf
saber <saberchabbbi@gmail.com>
2022-04-07 14:58:14 +0200

    Add project files.

---
+++ Scraping template 1 Thread1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Scraping_template_1_Thread1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
